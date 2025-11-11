13a1936614e821524d7f5ab1d8f4289880412dae
VladPyankov1337 <vladipyankov@yandex.ru>
2025-11-12 01:49:46 +0500

    Разработанный класс отправки сообщения на почту. Пьянков

--- Classes/SendMail.cs
+++ Classes/SendMail.cs
@@ -1,1 +1,19 @@
-﻿
+﻿using System.Net.Mail;
+using System.Net;
+
+namespace RegIN.Classes
+{
+    public class SendMail
+    {
+        public static void SendMessage(string Message, string To)
+        {
+            var smtpClient = new SmtpClient("smtp.yandex.ru")
+            {
+                Port = 587,
+                Credentials = new NetworkCredential("vladipyankov@yandex.ru", "rztynwwhutrqwlgr"),
+                EnableSsl = true,
+            };
+            smtpClient.Send("vladipyankov@yandex.ru", To, "Проект RegIn", Message);
+        }
+    }
+}
